2b2a75d01bfb6c4347df3fb7474b7a2c5f1b560a
Xueqian Hu <xueqianhu@Xueqians-MacBook-Pro.local>
2022-11-20 16:26:11 -0500

    fixed no buffer issue

--- kernel/virtio_net.c
+++ kernel/virtio_net.c
@@ -38,7 +38,7 @@ struct net {
   struct virtio_net_hdr ops[NUM];
 
   struct spinlock vnet_lock;
-} net;
+} net_send, net_recv;
 
 void initialize_queue(int queue_num);
 
@@ -46,17 +46,22 @@ void initialize_queue(int queue_num);
 void virtio_net_init(void *mac) {
   printf("virtio_net_init begin.\n");
 
-  initlock(&net.vnet_lock, "virtio_net");
+  initlock(&net_send.vnet_lock, "virtio_net");
+  initlock(&net_recv.vnet_lock, "virtio_net");
 
   uint32 status = 0;
 
-  net.desc = kalloc();
-  net.avail = kalloc();
-  net.used = kalloc();
-  if (!net.desc || !net.avail || !net.used) panic("virtio net kalloc fail");
-  memset(net.desc, 0, PGSIZE);
-  memset(net.avail, 0, PGSIZE);
-  memset(net.used, 0, PGSIZE);
+  for (int i = 0; i < 2; i++) {
+    struct net *net = i == 1 ? &net_send : &net_recv;
+    
+    net->desc = kalloc();
+    net->avail = kalloc();
+    net->used = kalloc();
+    if (!net->desc || !net->avail || !net->used) panic("virtio net kalloc fail");
+    memset(net->desc, 0, PGSIZE);
+    memset(net->avail, 0, PGSIZE);
+    memset(net->used, 0, PGSIZE);
+  }
 
   if (*R(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976 ||
       *R(VIRTIO_MMIO_VERSION) != 2 || *R(VIRTIO_MMIO_DEVICE_ID) != 1 ||
@@ -95,7 +100,10 @@ void virtio_net_init(void *mac) {
   initialize_queue(1);  // transmit queue
 
   // all NUM descriptors start out unused.
-  for (int i = 0; i < NUM; i++) net.free[i] = 1;
+  for (int i = 0; i < NUM; i++) {
+    net_send.free[i] = 1;
+    net_recv.free[i] = 1;
+  }
 
   // Tell device we're completely ready.
   status |= VIRTIO_CONFIG_S_DRIVER_OK;
@@ -114,11 +122,12 @@ void virtio_net_init(void *mac) {
 
 // find a free descriptor, mark it non-free, return its index.
 static int
-alloc_desc(void)
+alloc_desc(int send)
 {
+  struct net *net = send == 1 ? &net_send : &net_recv;
   for(int i = 0; i < NUM; i++){
-    if(net.free[i]){
-      net.free[i] = 0;
+    if(net->free[i]){
+      net->free[i] = 0;
       return i;
     }
   }
@@ -127,38 +136,40 @@ alloc_desc(void)
 
 // mark a descriptor as free.
 static void
-free_desc(int i)
+free_desc(int i, int send)
 {
+  struct net *net = send == 1 ? &net_send : &net_recv;
   if(i >= NUM)
     panic("virtio_net 1");
-  if(net.free[i])
+  if(net->free[i])
     panic("virtio_net 2");
-  net.desc[i].addr = 0;
-  net.free[i] = 1;
-  wakeup(&net.free[0]);
+  net->desc[i].addr = 0;
+  net->free[i] = 1;
+  wakeup(&net->free[0]);
 }
 
 // free a chain of descriptors.
 static void
-free_chain(int i)
+free_chain(int i, int send)
 {
+  struct net *net = send == 1 ? &net_send : &net_recv;
   while(1){
-    free_desc(i);
-    if(net.desc[i].flags & VIRTQ_DESC_F_NEXT)
-      i = net.desc[i].next;
+    free_desc(i, send);
+    if(net->desc[i].flags & VIRTQ_DESC_F_NEXT)
+      i = net->desc[i].next;
     else
       break;
   }
 }
 
 static int
-alloc2_desc(int *idx)
+alloc2_desc(int *idx, int send)
 {
   for(int i = 0; i < 2; i++){
-    idx[i] = alloc_desc();
+    idx[i] = alloc_desc(send);
     if(idx[i] < 0){
       for(int j = 0; j < i; j++)
-        free_desc(idx[j]); // free those already allocated descriptors
+        free_desc(idx[j], send); // free those already allocated descriptors
       return -1;
     }
   }
@@ -167,8 +178,10 @@ alloc2_desc(int *idx)
 
 /* send/receive data */
 int virtio_net_sr(const void *data, int len, int send) {
-  printf("virtio_net_sr: enter sr\n");
-  acquire(&net.vnet_lock);
+  struct net *net = send == 1 ? &net_send : &net_recv;
+
+  printf("virtio_net_sr: enter sr, send: %d\n", send);
+  acquire(&net->vnet_lock);
   // the spec says that legacy block operations use two
   // descriptors: one for package header, one for
   // the data
@@ -177,16 +190,16 @@ int virtio_net_sr(const void *data, int len, int send) {
   // If there are not enough free descriptors, sleep and wait.
   int idx[2];
   while(1){
-    if(alloc2_desc(idx) == 0) {
+    if(alloc2_desc(idx, send) == 0) {
       break;
     }
-    sleep(&net.free[0], &net.vnet_lock);
+    sleep(&net->free[0], &net->vnet_lock);
   }
 
   // format the two descriptors.
   // qemu's virtio-blk.c reads them.
 
-  struct virtio_net_hdr *buf0 = &net.ops[idx[0]];
+  struct virtio_net_hdr *buf0 = &net->ops[idx[0]];
 
   // set the header for this operation
   buf0->flags = 0; // read the disk
@@ -195,27 +208,27 @@ int virtio_net_sr(const void *data, int len, int send) {
 
   printf("virtio_net_sr: set descriptor\n");
   // set the first descriptor(header)
-  net.desc[idx[0]].addr = (uint64) buf0;
-  net.desc[idx[0]].len = sizeof(struct virtio_net_hdr);
-  net.desc[idx[0]].flags = VIRTQ_DESC_F_NEXT;
-  net.desc[idx[0]].next = idx[1];
+  net->desc[idx[0]].addr = (uint64) buf0;
+  net->desc[idx[0]].len = sizeof(struct virtio_net_hdr);
+  net->desc[idx[0]].flags = VIRTQ_DESC_F_NEXT;
+  net->desc[idx[0]].next = idx[1];
 
   // set the secode descriptor(data)
-  net.desc[idx[1]].addr = (uint64)data;
-  net.desc[idx[1]].len = len;
+  net->desc[idx[1]].addr = (uint64)data;
+  net->desc[idx[1]].len = len;
   if (send)
-    net.desc[idx[1]].flags = 0; // device read b->data
+    net->desc[idx[1]].flags = 0; // device read b->data
   else
-    net.desc[idx[1]].flags = VIRTQ_DESC_F_WRITE; // device writes b->data
-  net.desc[idx[1]].next = 0;
+    net->desc[idx[1]].flags = VIRTQ_DESC_F_WRITE; // device writes b->data
+  net->desc[idx[1]].next = 0;
 
   // avail->idx tells the device how far to look in avail->ring.
   // avail->ring[...] are desc[] indices the device should process.
   // we only tell device the first index in our chain of descriptors.
   // add this new running operation to avail ring[]
-  net.avail->ring[net.avail->idx % NUM] = idx[0];
+  net->avail->ring[net->avail->idx % NUM] = idx[0];
   __sync_synchronize();
-  net.avail->idx += 1;
+  net->avail->idx += 1;
 
   printf("virtio_net_sr: queue notify\n");
   *R(VIRTIO_MMIO_QUEUE_NOTIFY) = send ? 1 : 0; // value is queue number
@@ -224,27 +237,28 @@ int virtio_net_sr(const void *data, int len, int send) {
   int count = 0;
   while (1) {
     count++;
-    if (count % 1000000 == 0)
-      printf("virtio_net_sr: breaker\n");
+    //if (count % 1000000 == 0)
+      //printf("virtio_net_sr: breaker\n");
 
-    release(&net.vnet_lock);
-    acquire(&net.vnet_lock);
+    release(&net->vnet_lock);
+    acquire(&net->vnet_lock);
     // Device will put the finished operation into used ring.
     // We need to handle latest finished operation.
-    if((net.used_idx % NUM) != (net.used->idx % NUM)){
-      int id = net.used->ring[net.used_idx].id;
+    if((net->used_idx % NUM) != (net->used->idx % NUM)){
+      int id = net->used->ring[net->used_idx].id;
       printf("virtio_net_sr: has finish task\n");
       // make sure to handle its own id
       if (id != idx[0]) continue;
       printf("virtio_net_sr: get the id\n");
       // update use index and free the descriptor
-      net.used_idx = (net.used_idx + 1) % NUM;
-      free_chain(idx[0]);
+      net->used_idx = (net->used_idx + 1) % NUM;
+      free_chain(idx[0], send);
+      *R(VIRTIO_MMIO_INTERRUPT_ACK) = *R(VIRTIO_MMIO_INTERRUPT_STATUS) & 0x3;
       break;
     }
   }
 
-  release(&net.vnet_lock);
+  release(&net->vnet_lock);
 
   printf("virtio_net_sr: exit sr\n");
   return send ? 0 : len; 
@@ -260,6 +274,8 @@ int virtio_net_recv(void *data, int len) {
 }
 
 void initialize_queue(int queue_num) {
+  struct net *net = queue_num == 1 ? &net_send : &net_recv;
+
   *R(VIRTIO_MMIO_QUEUE_SEL) = queue_num;
   if (*R(VIRTIO_MMIO_QUEUE_READY))  // avoid reseting queue repeatly
     panic("virtio disk ready not zero");
@@ -270,12 +286,12 @@ void initialize_queue(int queue_num) {
 
   // point out the address to device. Specify high address and low address
   // separately. Disk will read/write these regions.
-  *R(VIRTIO_MMIO_QUEUE_DESC_LOW) = (uint64)net.desc;
-  *R(VIRTIO_MMIO_QUEUE_DESC_HIGH) = (uint64)net.desc >> 32;
-  *R(VIRTIO_MMIO_DRIVER_DESC_LOW) = (uint64)net.avail;
-  *R(VIRTIO_MMIO_DRIVER_DESC_HIGH) = (uint64)net.avail >> 32;
-  *R(VIRTIO_MMIO_DEVICE_DESC_LOW) = (uint64)net.used;
-  *R(VIRTIO_MMIO_DEVICE_DESC_HIGH) = (uint64)net.used >> 32;
+  *R(VIRTIO_MMIO_QUEUE_DESC_LOW) = (uint64)net->desc;
+  *R(VIRTIO_MMIO_QUEUE_DESC_HIGH) = (uint64)net->desc >> 32;
+  *R(VIRTIO_MMIO_DRIVER_DESC_LOW) = (uint64)net->avail;
+  *R(VIRTIO_MMIO_DRIVER_DESC_HIGH) = (uint64)net->avail >> 32;
+  *R(VIRTIO_MMIO_DEVICE_DESC_LOW) = (uint64)net->used;
+  *R(VIRTIO_MMIO_DEVICE_DESC_HIGH) = (uint64)net->used >> 32;
 
   /* Queue ready. Initialization finished*/
   *R(VIRTIO_MMIO_QUEUE_READY) = 0x1;
