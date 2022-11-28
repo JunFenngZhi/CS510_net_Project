6adefb54e02dea17cb208e44823240e3e20efc30
Xueqian Hu <xueqianhu@Xueqians-MacBook-Pro.local>
2022-11-27 20:32:17 -0500

    fix bugs in sysdns_api.c

--- kernel/sysdns_api.c
+++ kernel/sysdns_api.c
@@ -6,23 +6,23 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
-
+#include "lwip/include/lwip/opt.h"
 
 uint64 sys_dns_api_gethostbyname(void) {
     int n;
     char host[DNS_MAX_NAME_LENGTH + 1];
 
-    uint32* ip_addr;
+    uint64 ip_addr;
 
     if((n = argstr(0, host, MAXPATH)) < 0) {
         panic("fail to get hostname.");
     }
 
-    if (argaddr(1, ip_addr) < 0) {
+    if (argaddr(1, &ip_addr) < 0) {
         panic("fail to get ip_addr.");
     }
 
     printf("dns: hostname: %s\n", host);
 
-    return dns_api_gethostbyname((const char *)host, ip_addr);
+    return dns_api_gethostbyname((const char *)host, (uint32*)ip_addr);
 }
